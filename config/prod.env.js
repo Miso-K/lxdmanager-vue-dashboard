6455af4dfe9bfde67dfca2d5e3e82ca6fa0609b7
Michal Kolarik <kolarik@skhosting.eu>
2020-05-01 01:01:58 +0200

    Fix API_BASE_URL path

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ var dotenv = require('dotenv').config({ path: 'static/.env'})
 
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: `"${dotenv.parsed.API_BASE_URL}"`,
+  API_BASE_URL: `"${dotenv.parsed.API_BASE_URL}/api/v1"`,
   API_BASE_WS_URL: `"${dotenv.parsed.API_BASE_WS_URL}"`
 }
