1754fbee52af3def0773be71a8830930fd9fff82
Michal Kolarik <kolarik@skhosting.eu>
2020-07-09 15:14:08 +0200

    Fix price calculation

--- src/components/Instance/Settings.vue
+++ src/components/Instance/Settings.vue
@@ -160,7 +160,7 @@
                 </v-flex>
                 <v-flex xs3>
                   <v-select
-                    :items=periodes
+                    :items=getPeriodes
                     v-model="period"
                     item-text="text"
                     item-value="value"
@@ -281,6 +281,8 @@
         return this.$store.getters.appconfig.cpu;
       },
       getMemory() {
+        console.log(this.$store.getters.appconfig);
+        console.log(this.periodes);
         return this.$store.getters.appconfig.memory;
       },
       getStorage() {
@@ -300,6 +302,16 @@
       },
       getProductionName() {
         return this.$store.getters.appconfig.app.production_name;
+      },
+      getPeriodes() {
+        const pers = this.periodes;
+        const config = this.$store.getters.appconfig;
+        pers[0].value = config.price.discount_month;
+        pers[1].value = config.price.discount_months;
+        pers[2].value = config.price.discount_halfyear;
+        pers[3].value = config.price.discount_year;
+        pers[4].value = config.price.discount_years;
+        return pers;
       },
       calcPrice: {
         get() {
