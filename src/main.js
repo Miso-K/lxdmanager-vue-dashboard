115e149b5c029c9024ab458a8736f0f95773f5b9
Miso-K <mikki.kol@gmail.com>
2021-11-04 18:33:53 +0100

    Fix auth redirect

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import './libraries/utils/axios'
+import plugins from './libraries/plugins/index'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import router from './libraries/router'
 import store from './libraries/store/index'
-import i18n from './libraries/i18n/index';
+import i18n from './libraries/i18n/index'
 // import Vuex from './libraries/store/index'
 // import Layout from './layouts/Layout.vue'
 
 // Vue.component('Layout', Layout)
 
 Vue.config.productionTip = false
+
+Vue.use(plugins);
 
 new Vue({
   vuetify,
